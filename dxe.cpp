da783908752ebf8657263da085ad0676e19de594
VenalexD <52059597+VenalexD@users.noreply.github.com>
2020-03-29 19:34:40 -0700

    Update dxe.cpp
    Added comments

--- dxe.cpp
+++ dxe.cpp
@@ -14,24 +14,27 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+        // Given two arguments were given (executable program and file to be read)
         if(argc == 2)
         {
                 ifstream inFile;
-                string file = argv[1];
+                string file = argv[1];  // Assign variable name "file" to second argument (file to be read)
                 inFile.open(file);
                 
+                // If no file is opened, either does NOT exist or NOT readable
                 if(!inFile)
                 {
                         cout << "Unable to open file: " << file << endl;
-                        return -1;
+                        return -1;      // Exits program
                 }
 
                 inFile.close();
         }
 
+        // Given two arguments were NOT given (executable program and file to be read)
         else
         {
-                cout << "Usage: " << argv[0] << " <filename>" << endl;
+                cout << "Usage: " << argv[0] << " <filename>" << endl;  // Shows the correct usage of the program and arguments
         }
         
         return 0;
