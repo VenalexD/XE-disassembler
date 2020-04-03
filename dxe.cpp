dfa1f5483407b33649df69678b4d86beea2fffb8
VenalexD <52059597+VenalexD@users.noreply.github.com>
2020-04-02 20:28:08 -0700

    Update dxe.cpp
    Starting translation from object code to source code. (Only first line of source done)

--- dxe.cpp
+++ dxe.cpp
@@ -9,8 +9,37 @@ Filename: dxe.cpp
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <math.h>
 
 using namespace std;
+
+int hexToDec(unsigned char hex[])
+{
+        int hexInt[6];  // Holds int values of hex
+        int sum = 0;    // Holds sum of hex values
+        for(int i=0; i<6; i++)
+        {
+                if(hex[i] >= 48 && hex[i] <= 57)
+                {
+                        hexInt[i] = (int) hex[i] - 48;  // Converts char to int value and subtracts 48
+                }
+
+                else if(hex[i] >= 65 && hex[i] <= 70)
+                {
+                        hexInt[i] = (int) hex[i] - 55;  // Converts char to int value and subtracts 55
+                }
+
+                else
+                {
+                        cout << "Invalid Hex value found" << endl;
+                        exit(1);      // Exits program
+                }
+                
+                sum += pow(16, i)*hexInt[i];
+        }
+
+        return sum;
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,19 +52,35 @@ int main(int argc, char *argv[])
                 string symF = file + ".sym";    // Adds sym extension to symbol file
                 string sicF = file + ".sic";    // Adds sic extension to source file
 
-                // Creates and opens source file
-                ofstream outFile(sicF);
-
-                
-
                 inFile.open(objF);
-                outFile << "test";
                 // If no file is opened, either does NOT exist or NOT readable
                 if(!inFile)
                 {
                         cout << "Unable to open file: " << file << endl;
                         return -1;      // Exits program
                 }
+
+                // Creates and opens source file
+                ofstream outFile(sicF);
+                char c; // Placeholder for character being read
+                inFile.get(c);  // Skips first character 'H'
+                // Loops through 6 characters of program name
+                for(int i=0; i<6; i++)
+                {
+                        inFile.get(c);  // Gets character from input file
+                        outFile << c;   // Writes each charatcer to output file
+                }
+
+                unsigned char x[6];       // Int array to hold hex values
+                // Loops through 6 hex digits of object program starting address
+                for(int j=0; j<6; j++)
+                {
+                        inFile.get(c);
+                        x[j] = c;
+                }
+
+                outFile << "   START   " << hexToDec(x);
+
                 outFile.close();
                 inFile.close();
         }
