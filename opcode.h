5870597bf0a3a902af4c8102a0835b433e251fe0
VenalexD <52059597+VenalexD@users.noreply.github.com>
2020-04-14 18:36:59 -0700

    opcode header file

---
+++ opcode.h
@@ -0,0 +1,15 @@
+/*
+Authors: Alex Gonzalez, Juan Pina-Sanz, Mikhail Mineev, Mark Khoury
+Users: cssc1967, cssc1923, cssc1921, cssc1964
+Class: CS 530, Spring 2020
+Assignment 2, XE Dissasembler
+Filename: opcode.h
+*/
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+// Protoype for buildObjMap
+void buildObjMap(map<int, string> &obj_map);
