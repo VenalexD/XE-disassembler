46b567db74e92356d2782aa0c136f307f8cfcbba
VenalexD <52059597+VenalexD@users.noreply.github.com>
2020-04-14 18:36:23 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+Authors: Alex Gonzalez, Juan Pina-Sanz, Mikhail Mineev, Mark Khoury
+Users: cssc1967, cssc1923, cssc1921, cssc1964
+Class: CS 530, Spring 2020
+Assignment 2, XE Dissasembler
+Filename: main.cpp
+*/
+
+#include "dxe.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+        // Given two arguments were given (executable program and file to be read)
+        if(argc == 2)
+        {
+            // Instantiates XED form dxe class
+            dxe XED;
+            XED.openF(argv[1]); // Calls open function with command line argument
+        }
+
+        else
+        {
+                cout << "Usage: " << argv[0] << " <filename>" << endl;  // Shows the correct usage of the program and arguments
+        }
+        
+        return 0;
